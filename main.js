f8c6923cfae73442300646c9c2cdb01aeb363229
Eduardo <e.git@xor.cl>
2021-04-04 20:11:36 -0400

    show only fields with div

--- main.js
+++ main.js
@@ -39,14 +39,16 @@ window.onload = function() {
             } else {
                 for (const [prop, value] of Object.entries(data.data)) {
                     let data_tr = document.querySelector(`#${prop}`);
-                    let warning = data_tr.querySelector(".warning");
-                    let ok = data_tr.querySelector(".ok");
-                    if (value === true) {
-                        warning.classList.remove("hidden");
-                        ok.classList.add("hidden");
-                    } else {
-                        ok.classList.remove("hidden");
-                        warning.classList.add("hidden");
+                    if (data_tr !== null) {
+                        let warning = data_tr.querySelector(".warning");
+                        let ok = data_tr.querySelector(".ok");
+                        if (value === true) {
+                            warning.classList.remove("hidden");
+                            ok.classList.add("hidden");
+                        } else {
+                            ok.classList.remove("hidden");
+                            warning.classList.add("hidden");
+                        }    
                     }
                 }
                 results.classList.remove("hidden");
